b5e6e650d8452221bdb12d94b088ff34b2ebbef3
byungminkim <byungminkim12@gmail.com>
2021-05-26 22:43:44 +0900

    2021.05.26 영화 웹 사이트 만들기 클론코딩

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,10 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+export const API_KEY = "a32f03853f78f07a540370ea96e1c660"
+
